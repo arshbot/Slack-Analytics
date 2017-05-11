2256f3d1d4727d70f52ce998a3ccbe44dfada4f2
Harsha Goli <harshagoli#gmail.com>
2017-05-11 12:18:58 -0400

    Initialized setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chatbot_analytics',
+      version='0.1',
+      description='Provides analytics for activity on chat servers monitored by a chatbot',
+      url='http://github.com/arshbot/slack-analytics',
+      author='Harsha Goli',
+      author_email='harshagoli@gmail.com',
+      license='MIT',
+      packages=['chatbot_analytics'],
+      zip_safe=False)
