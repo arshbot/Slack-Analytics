2e160697fa2afda500115b2912b095481679ddf3
Harsha Goli <harshagoli#gmail.com>
2017-05-11 12:07:36 -0400

    Moved PB stuff to PB folder

--- scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-commands = {}
-for module in os.listdir(os.path.dirname(__file__)):
-    mod_name = module[:-3]
-    mod_ext = module[-3:]
-
-    if module == '__init__.py' or mod_ext != '.py':
-        continue
-    commands[mod_name] = __import__(mod_name, locals(), globals())
